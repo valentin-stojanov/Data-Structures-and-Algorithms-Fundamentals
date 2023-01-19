019168141b42c4f7a521b2d72275a10684c05c5b
valentin-stojanov <valentin_stojanov@abv.bg>
2023-01-19 13:34:19 +0200

    Implemented some methods to ArrayList.

--- Data-Structures/LinearDS/src/main/java/Main.java
+++ Data-Structures/LinearDS/src/main/java/Main.java
@@ -1,5 +1,17 @@
+import implementations.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("a");
+        strings.add("b");
+        strings.add("c");
+        strings.add("d");
+//        strings.add("e");
+
+        strings.add(3, "f");
+        System.out.println();
 
     }
 }
--- Data-Structures/LinearDS/src/main/java/implementations/ArrayList.java
+++ Data-Structures/LinearDS/src/main/java/implementations/ArrayList.java
@@ -2,29 +2,60 @@ package implementations;
 
 import interfaces.List;
 
-import java.util.Arrays;
 import java.util.Iterator;
 
 public class ArrayList<E> implements List<E> {
+    private static final int INITIAL_SIZE = 4;
+    private int size;
+    private Object[] elements;
+
+    public ArrayList() {
+        this.size = 0;
+        this.elements = new Object[INITIAL_SIZE];
+    }
+
+    public ArrayList(int initialSize) {
+        this.size = 0;
+        this.elements = new Object[initialSize];
+    }
 
     @Override
     public boolean add(E element) {
-        return false;
+        ensureCapacity();
+        this.elements[size++] = element;
+
+        return true;
     }
 
     @Override
     public boolean add(int index, E element) {
-        return false;
+        if (!isValidIndex(index)) {
+            return false;
+        }
+        ensureCapacity();
+
+        for (int i = this.size; i >= index; i--) {
+            this.elements[i + 1] = this.elements[i];
+        }
+
+        this.elements[index] = element;
+        this.size++;
+
+        return true;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        validateIndex(index);
+        return (E) this.elements[index];
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        validateIndex(index);
+        E temp = (E) this.elements[index];
+        this.elements[index] = element;
+        return temp;
     }
 
     @Override
@@ -55,5 +86,27 @@ public class ArrayList<E> implements List<E> {
     @Override
     public Iterator<E> iterator() {
         return null;
+    }
+
+    private void ensureCapacity() {
+        if (this.size < this.elements.length) {
+            return;
+        }
+//        keep in mind that the max array size is Integer.MAX_VALUE -2!!!
+        Object[] temp = new Object[elements.length * 2];
+        for (int i = 0; i < this.elements.length; i++) {
+            temp[i] = this.elements[i];
+        }
+        elements = temp;
+    }
+
+    private boolean isValidIndex(int index) {
+        return 0 <= index && index < this.size;
+    }
+
+    private void validateIndex(int index) {
+        if (!isValidIndex(index)) {
+            throw new IndexOutOfBoundsException("Invalid index " + index);
+        }
     }
 }
