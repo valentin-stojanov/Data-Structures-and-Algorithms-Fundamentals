7cec6188d8c15914f3c3a7a43b958afe0eb51798
Valentin <valentin_stojanov@abv.bg>
2023-01-31 21:27:30 +0200

    Added orderDfs(), addChild(), removeNode().

--- Data-Structures/TreesRepresentation/src/main/java/implementations/Tree.java
+++ Data-Structures/TreesRepresentation/src/main/java/implementations/Tree.java
@@ -2,10 +2,7 @@ package implementations;
 
 import interfaces.AbstractTree;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Tree<E> implements AbstractTree<E> {
 
@@ -26,11 +23,14 @@ public class Tree<E> implements AbstractTree<E> {
     @Override
     public List<E> orderBfs() {
         List<E> bfsList = new ArrayList<>();
+        if (this.key == null) {
+            return bfsList;
+        }
         Queue<Tree<E>> queue = new ArrayDeque<>();
         Tree<E> tree = this;
 
         queue.offer(tree);
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
 
             Tree<E> currentTree = queue.peek();
 
@@ -46,16 +46,78 @@ public class Tree<E> implements AbstractTree<E> {
 
     @Override
     public List<E> orderDfs() {
-        return null;
+        List<E> dfsList = new ArrayList<>();
+        Tree<E> tree = this;
+
+//        Queue<Tree<E>> queue = new ArrayDeque<>();
+//        queue.offer(tree);
+//        while (!queue.isEmpty()){
+//            Tree<E> current = queue.poll();
+//            dfsList.add(0, current.key);
+//
+//            for (int i = current.children.size() -1; i >= 0 ; i--) {
+//                queue.offer(current.children.get(i));
+//            }
+//        }
+
+        this.dfs(tree, dfsList);
+
+        return dfsList;
+    }
+
+    private void dfs(Tree<E> tree, List<E> dfsList) {
+        for (Tree<E> child : tree.children) {
+            this.dfs(child, dfsList);
+        }
+        dfsList.add(tree.key);
     }
 
     @Override
     public void addChild(E parentKey, Tree<E> child) {
+        Tree<E> node = findNodeBfs(parentKey);
+        if (node != null) {
+            child.parent = node;
+            node.children.add(child);
+        }
 
+    }
+
+    private Tree<E> findNodeBfs(E key) {
+        Queue<Tree<E>> queue = new ArrayDeque<>();
+        Tree<E> node = null;
+        Tree<E> tree = this;
+
+        queue.offer(tree);
+        while (!queue.isEmpty()) {
+            Tree<E> currentTree = queue.peek();
+
+            for (Tree<E> child : currentTree.children) {
+                queue.offer(child);
+            }
+
+            if (key.equals(queue.poll().key)) {
+                node = currentTree;
+            }
+        }
+
+        return node;
     }
 
     @Override
     public void removeNode(E nodeKey) {
+        Tree<E> nodeToRemove = findNodeBfs(nodeKey);
+
+        if (nodeToRemove != null) {
+            if (nodeToRemove.equals(this)) {
+                this.children = null;
+                this.key = null;
+                return;
+            }
+            Tree<E> parent = nodeToRemove.parent;
+            parent.children.remove(nodeToRemove);
+            nodeToRemove.parent = null;
+        }
+
 
     }
 
