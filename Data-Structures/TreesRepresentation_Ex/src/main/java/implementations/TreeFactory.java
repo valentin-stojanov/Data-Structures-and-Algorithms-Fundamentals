473cd00d1d16a4b61d068488f0a9eda72ddcd1d7
Valentin <valentin_stojanov@abv.bg>
2023-02-05 21:26:24 +0200

    Added tree creation.

--- Data-Structures/TreesRepresentation_Ex/src/main/java/implementations/TreeFactory.java
+++ Data-Structures/TreesRepresentation_Ex/src/main/java/implementations/TreeFactory.java
@@ -1,7 +1,10 @@
 package implementations;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TreeFactory {
     private Map<Integer, Tree<Integer>> nodesByKeys;
@@ -11,19 +14,42 @@ public class TreeFactory {
     }
 
     public Tree<Integer> createTreeFromStrings(String[] input) {
-        return null;
+
+        for (String tuple : input) {
+            int[] nodeChild = Arrays.stream(tuple.split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            Tree<Integer> parentNode = createNodeByKey(nodeChild[0]);
+            Tree<Integer> childNode = createNodeByKey(nodeChild[1]);
+
+            addEdge(parentNode, childNode);
+
+            this.nodesByKeys.putIfAbsent(nodeChild[0], parentNode);
+            this.nodesByKeys.putIfAbsent(nodeChild[1], childNode);
+
+            nodesByKeys.get(nodeChild[1]).setParent(this.nodesByKeys.get(nodeChild[0]));
+
+        }
+
+        return this.getRoot();
     }
 
     private Tree<Integer> getRoot() {
+        for (Tree<Integer> node : nodesByKeys.values()) {
+            if (node.getParent() == null) {
+                return node;
+            }
+        }
         return null;
     }
 
     public Tree<Integer> createNodeByKey(int key) {
-        return null;
+        return new Tree<>(key);
     }
 
-    public void addEdge(int parent, int child) {
-
+    public void addEdge(Tree<Integer> parent, Tree<Integer> child) {
+        child.setParent(parent);
     }
 }
 
