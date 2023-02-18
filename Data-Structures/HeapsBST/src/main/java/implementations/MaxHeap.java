1a82d026f75c18622348d499005665bc5bc0ff29
Valentin <valentin_stojanov@abv.bg>
2023-02-18 23:24:47 +0200

    Implemented MaxHeap.

--- Data-Structures/HeapsBST/src/main/java/implementations/MaxHeap.java
+++ Data-Structures/HeapsBST/src/main/java/implementations/MaxHeap.java
@@ -8,18 +8,58 @@ import java.util.List;
 
 public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
 
+    private List<E> elements ;
+
+    public MaxHeap() {
+        this.elements = new ArrayList<>();
+    }
+
     @Override
     public int size() {
-        return 0;
+        return this.elements.size();
     }
 
     @Override
     public void add(E element) {
+        this.elements.add(element);
+        this.heapifyUp(this.size() - 1);
+    }
 
+    private void heapifyUp(int index) {
+        int currentIndex = index;
+        int parentIndex = getParent(currentIndex);
+        E currentElement = this.elements.get(index);
+        E parentElement = this.elements.get(parentIndex);
+
+        while (!isMaxHeap(parentElement, currentElement)){
+            swap(parentIndex, currentIndex);
+            currentIndex = parentIndex;
+            parentIndex = getParent(currentIndex);
+            currentElement = this.elements.get(currentIndex);
+            parentElement = this.elements.get(parentIndex);
+        }
+
+    }
+
+    private void swap(int parentElementIndex, int currentElementIndex) {
+       E parentElement = this.elements.get(parentElementIndex);
+       this.elements.set(parentElementIndex, this.elements.get(currentElementIndex));
+       this.elements.set(currentElementIndex, parentElement);
+    }
+
+    private boolean isMaxHeap(E parentElement, E currentElement) {
+        return parentElement.compareTo(currentElement) >= 0;
+    }
+
+    private int getParent(int index) {
+        return (index - 1) / 2;
     }
 
     @Override
     public E peek() {
-        return null;
+        if (this.size() == 0){
+            throw new IllegalStateException();
+        }
+        return this.elements.get(0);
     }
 }
