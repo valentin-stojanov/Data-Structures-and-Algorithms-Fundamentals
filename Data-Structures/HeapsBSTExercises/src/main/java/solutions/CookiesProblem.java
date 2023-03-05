2ec113323a339ac5bb28a25b503f694e991b8093
Valentin <valentin_stojanov@abv.bg>
2023-03-05 17:59:03 +0200

    Implemented CookiesProblem.

--- Data-Structures/HeapsBSTExercises/src/main/java/solutions/CookiesProblem.java
+++ Data-Structures/HeapsBSTExercises/src/main/java/solutions/CookiesProblem.java
@@ -1,7 +1,28 @@
 package solutions;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class CookiesProblem {
-    public Integer solve(int k, int[] cookies) {
-        return null;
+    public Integer solve(int requiredSweetness, int[] cookiesSweetness) {
+        Queue<Integer> cookies = new PriorityQueue<>();
+        for (Integer cookie : cookiesSweetness) {
+            cookies.add(cookie);
+        }
+
+        int counter = 0;
+        int currentMinSweetness = cookies.peek();
+        while (currentMinSweetness < requiredSweetness && cookies.size() >= 2) {
+            Integer leastSweetCookie = cookies.poll();
+            Integer secondLeastSweetCookie = cookies.poll();
+            Integer newCookie = leastSweetCookie + 2 * secondLeastSweetCookie;
+
+            cookies.offer(newCookie);
+
+            currentMinSweetness = cookies.peek();
+            counter++;
+        }
+
+        return currentMinSweetness >= 7 ? counter : -1;
     }
 }
