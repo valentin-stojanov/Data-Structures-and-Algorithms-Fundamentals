b04f4ddd34d12c038575f0bec507d91bfa493bca
Valentin <valentin_stojanov@abv.bg>
2023-03-07 23:05:09 +0200

    Added Recursion and Backtracking to Algorithms.

---
+++ Algorithms/Recursion-And-Backtracking/src/main/java/GeneratingVectors.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class GeneratingVectors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+        Integer[] vector = new Integer[size];
+
+        fillVector(vector, 0);
+    }
+
+    private static void fillVector(Integer[] vector, int index) {
+        if (index == vector.length) {
+            print(vector);
+            return;
+        }
+
+        for (int i = 0; i <= 1; i++) {
+            vector[index] = i;
+            fillVector(vector, index + 1);
+        }
+    }
+
+    private static void print(Integer[] vector) {
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector[i]);
+        }
+        System.out.println();
+    }
+}
---
+++ Algorithms/Recursion-And-Backtracking/src/main/java/RecursiveDrawing.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class RecursiveDrawing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+
+        drawFigure(size);
+    }
+
+    private static void drawFigure(int size) {
+        if (size <= 0) {
+            return;
+        }
+        printLineOfCharactersWithGivenSize('*', size);
+        //The code above is executed before recursion
+        drawFigure(size - 1);
+        //The code below is executed after recursion
+
+        printLineOfCharactersWithGivenSize('#', size);
+    }
+
+    private static void printLineOfCharactersWithGivenSize(char character, int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(character);
+        }
+        System.out.println();
+    }
+
+}
